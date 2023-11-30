5b892e2069ff1aed81c1b5da754d2115f8d51548
Maximus019BG <123507237+Maximus019BG@users.noreply.github.com>
2023-11-30 18:23:59 +0200

    Update app.py

--- index/app.py
+++ index/app.py
@@ -7,6 +7,18 @@ result = subprocess.run(['php', php_file_path], capture_output=True, text=True)
 
 # Print the output of the PHP script
 print(result.stdout)
+# main.py
+from myapp import create_asgi_app
+
+application = create_asgi_app()
+# main.py
+try:
+    from myapp import create_asgi_app
+    application = create_asgi_app()
+except Exception as e:
+    print(f"Error importing 'main': {e}")
+    raise
+
 
 # Check for any errors
 if result.returncode != 0:
