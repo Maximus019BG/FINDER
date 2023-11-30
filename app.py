db10b7681788aa63c130a808072463cb0f4114e6
Maximus019BG <123507237+Maximus019BG@users.noreply.github.com>
2023-11-30 18:24:51 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+import subprocess
+
+php_file_path = 'index/index.php'
+
+# Run PHP file
+result = subprocess.run(['php', php_file_path], capture_output=True, text=True)
+
+# Print the output of the PHP script
+print(result.stdout)
+# main.py
+from myapp import create_asgi_app
+
+application = create_asgi_app()
+# main.py
+try:
+    from myapp import create_asgi_app
+    application = create_asgi_app()
+except Exception as e:
+    print(f"Error importing 'main': {e}")
+    raise
+
+
+# Check for any errors
+if result.returncode != 0:
+    print(f"Error: {result.stderr}")
